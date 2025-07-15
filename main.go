e8240af228cc38fbb505bc79c660a19dcdaaada8
Atheeque <159514505+AtheequeFurkhan@users.noreply.github.com>
2025-07-15 12:26:03 +0530

    Update : update the create function

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package crudapi
+package main
 
 import (
 	"encoding/json"
@@ -25,7 +25,7 @@ type Director struct{
 
 var movies []Movie
 
-func getMovie(w http.ResponseWriter, r *http.Request){
+func getMovies(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type","application/json")
 	json.NewEncoder(w).Encode(movies)
 
@@ -54,11 +54,23 @@ func createMovie(w http.ResponseWriter,r *http.Request){
 }
 
 func updateMovie(w http.ResponseWriter,r *http.Request){
+
 	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)
+	for index,item := range movies{
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = strconv.Itoa(rand.Intn(1000000))
+			movies = append(movies,movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
 
 }
 
-func getMovies(w http.ResponseWriter, r *http.Request){
+func getMovie(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type","application/json")
 	params := mux.Vars(r) 
 	for _, item:= range movies{
@@ -67,6 +79,7 @@ func getMovies(w http.ResponseWriter, r *http.Request){
 		}
 	} 
 }
+
 
 func main(){
 	r := mux.NewRouter()
@@ -80,9 +93,9 @@ func main(){
 
 
 	r.HandleFunc("/movies",getMovies).Methods("GET")
-	r.HandleFunc("movies/{id}",getMovie).Methods("GET")
+	r.HandleFunc("/movies/{id}",getMovie).Methods("GET")
 	r.HandleFunc("/movies",createMovie).Methods("POST")
-	r.HandleFunc("movies/{id}",updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}",updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}",deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting Server at 8080!")
